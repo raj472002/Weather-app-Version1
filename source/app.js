c4c997f5b4837242424b992ddcda833f570f9862
raj <baindraraj18@gmail.com>
2020-06-12 13:12:20 +0530

    set up app for heroku

--- source/app.js
+++ source/app.js
@@ -9,7 +9,7 @@ const forecast=require('./utils/forecast')
 
 const app=express()
 
-
+const port=process.env.PORT || 3000
 
 // console.log(__dirname)
 // console.log(path.join(__dirname,'../public'))
@@ -164,6 +164,6 @@ app.get('*',(req,res)=>{
 
 
 
-app.listen(3000,()=>{
-    console.log('the  server started on port 3000')
+app.listen(port,()=>{
+    console.log('the  server started on port '+port)
 })
